55c6bcfd29923b0b9c705b82bedc332f9979f96a
AlejandroH91 <hernandez.razo.alejandro@iescamas.es>
2026-02-24 08:51:09 +0100

    Añadido botón hamburguesa

--- proyecto-diseño/src/componentes/Cabecera.jsx
+++ proyecto-diseño/src/componentes/Cabecera.jsx
@@ -4,9 +4,14 @@ import Reloj from "./Reloj";
 function Cabecera() {
   return (
     <header className="bg-success text-white fixed-top">
-      <div className="container">
+      <div className="container navbar navbar-dark navbar-expand-md d-block">
 
-        <div className="row py-3">
+        <div className="row py-3 align-items-center">
+          <div className="col-12 d-md-none text-center mb-3">
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menuNavegacion" aria-controls="menuNavegacion" aria-expanded="false" aria-label="Navegación">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+          </div>
           <div className="d-none d-md-block col-12 col-md-4 text-start">
             <h1>AHR</h1>
           </div>
@@ -19,36 +24,37 @@ function Cabecera() {
             </h1>
           </div>
         </div>
+        <div className="collapse navbar-collapse" id="menuNavegacion">
+          <div className="row py-2 justify-content-center w-100">
+            <div className="col-6 col-md-2 text-center">
+              <Link className="text-white text-decoration-none" to="/">
+               <h3>Bienvenida</h3> 
+              </Link>
+            </div>
 
-      
-        <div className="row py-2 justify-content-center">
-          <div className="col-6 col-md-2 text-center">
-            <Link className="text-white text-decoration-none" to="/">
-             <h3>Bienvenida</h3> 
-            </Link>
-          </div>
+            <div className="col-6 col-md-2 text-center">
+              <Link className="text-white text-decoration-none" to="/ajax">
+                 <h3>Perros API</h3>
+              </Link>
+            </div>
 
-          <div className="col-6 col-md-2 text-center">
-            <Link className="text-white text-decoration-none" to="/ajax">
-               <h3>Perros API</h3>
-            </Link>
-          </div>
+            <div className="col-6 col-md-2 text-center">
+              <Link className="text-white text-decoration-none" to="/local">
+                 <h3>LocalStorage</h3>
+              </Link>
+            </div>
 
-          <div className="col-6 col-md-2 text-center">
-            <Link className="text-white text-decoration-none" to="/local">
-               <h3>LocalStorage</h3>
-            </Link>
+            <div className="col-6 col-md-2 text-center">
+              <Link className="text-white text-decoration-none" to="/ejemplo5">
+                <h3>Subrouting</h3>
+              </Link>
+            </div>
           </div>
-
-          <div className="col-6 col-md-2 text-center">
-            <Link className="text-white text-decoration-none" to="/ejemplo5">
-              <h3>Subrouting</h3>
-            </Link>
-          </div>
+          
         </div>
 
       </div>
-    </header>
+</header>
   );
 }
 
